c62abf5a0b1ed2ef0279b1e53dfc5c9b4e8a767b
Erik Kjellberg <erikkjellberg@erikspc>
2021-05-09 13:53:02 +0200

    Implementerade den nya algoritmen för avståndsberäkning

--- virus_sim.py
+++ virus_sim.py
@@ -1,17 +1,17 @@
 import pygame
+import math
 import numpy as np
 import numpy.random as rnd
 import matplotlib.pyplot as plt
 
+
 pygame.init()
+font1 = pygame.font.SysFont("courier", 24)
+font2 = pygame.font.SysFont("Arial", 12, italic=True, bold=True)
 width, height = 1000, 600
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 fps = 15
-
-font1 = pygame.font.SysFont("courier", 24)
-font2 = pygame.font.SysFont("Arial", 12, italic=True, bold=True)
-
 
 def distance(x1, x2, y1, y2):
     return np.sqrt((y2-y1)**2+(x2-x1)**2)
@@ -26,7 +26,7 @@ class Person:
         self.rot_vel = om
         self.state = state
         self.current_sick_time = 0
-        self.sick_time = 300
+        self.sick_time = rnd.normal(300, 50)
         self.tp_chance = 0.01
         self.tp_cooldown = 15
         self.tp_time = 0
@@ -35,8 +35,8 @@ class Person:
         self.tp_radius = tp_radius
         self.tpd = False
         self.last_pos = [self.x, self.y]
-        self.mat_pos = [0,0]
-        self.death_risk = 0.001
+        self.matrix_pos = [0,0]
+        self.death_risk = 0.00001
 
     def update(self, area):
         if self.state != 3:
@@ -70,7 +70,7 @@ class Person:
                     return -1
 
             #Tillfriskning
-            if self.current_sick_time == self.sick_time and self.state != 2:
+            if self.current_sick_time >= self.sick_time and self.state != 2:
                 self.state = 2
                 return 1
         return 0
@@ -100,57 +100,55 @@ class Person:
                 self.tp_time += 1
 
 
-    def draw(self, area, colors):
+    def draw(self, area, colors, distance):
         #Ritar cirklar i olika färger beroende på tillstånd
+        pygame.draw.circle(screen, (150,150,150), (int(area.x + self.x), int(area.y + self.y)), int(distance/2), 1)
         pygame.draw.circle(screen, colors[self.state], (int(area.x + self.x), int(area.y + self.y)), 3)
 
-class pop_matrix: #skapar matris för avståndsbedömning
-    def __init__(self, pop_size, area, safe_distance):
+class PopulationMatrix: #skapar matris för avståndsbedömning
+    def __init__(self, area, safe_distance):
         self.mat_pop = []
         self.area = area
-        self.pop_size = pop_size
         self.safe_distance = safe_distance
-        self.width = area[0]//safe_distance
-        self.height = area[1]//safe_distance
+        self.width = math.ceil(area.width//safe_distance)
+        self.height = math.ceil(area.height//safe_distance)
         for i in range(self.width):
-            self.matpop.append([])
+            self.mat_pop.append([])
             for j in range(self.height):
-                self.matpop[j].append([])
+                self.mat_pop[i].append([])
 
-    def add_pop(pop_list): #lägger in pop i matrisen
+    def add_pop(self, pop_list): #lägger in pop i matrisen
         for person in pop_list:
-            person.mat_pos = [person.x//self.safe_distance, person.y//self.safe_distance]
-            self.mat_pop[person.matpos[0]][person.mat_pos[1]].append(person)
+            self.add_person(person)
 
-    def add_person(person): #lägger till en person i matrisen
-        person.mat_pos = [person.x//self.safe_distance, person.y//self.safe_distance]
+    def add_person(self,person): #lägger till en person i matrisen
+        person.mat_pos = [math.floor(person.x//self.safe_distance), math.floor(person.y//self.safe_distance)]
+        if person.mat_pos[0] > self.width-1:
+            person.mat_pos[0] = self.width-1
+        if person.mat_pos[1] > self.height-1:
+            person.mat_pos[1] = self.height-1
         self.mat_pop[person.mat_pos[0]][person.mat_pos[1]].append(person)
 
-    def update_person(person): #uppdaterar personens matrix pos efter att positionen har ändrats
+    def update_person(self, person): #uppdaterar personens matrix pos efter att positionen har ändrats
         self.mat_pop[person.mat_pos[0]][person.mat_pos[1]].remove(person)
-        pop_matrix.add_person(person)
+        person.mat_pos = [person.x//self.safe_distance, person.y//self.safe_distance]
+        self.add_person(person)
 
-    def check_distance(pop): #returnernar ett set med par med folk som är för nära varandra
+    def check_distance(self,pop): #returnernar ett set med par med folk som är för nära varandra
         too_close = set()
         for person in pop:
-            matrix_pos = person.matrix_pos
-            for i in range(matrix_pos[0] - 1, matrix_pos[0] + 2):
-                for j in range(matrix_pos[1] - 1, matrix_pos[1] + 2):
-                    if len(mat_pop[i][j]) != 0:
-                        for person_2 in mat_pop[i][j]:
+            matrix_pos = person.mat_pos
+            for i in range(matrix_pos[0] - 1, min(self.width, matrix_pos[0] + 2)):
+                for j in range(matrix_pos[1] - 1, min(self.height, matrix_pos[1] + 2)):
+                    if len(self.mat_pop[i][j]) != 0:
+                        for person_2 in self.mat_pop[i][j]:
                             if distance(person.x, person_2.x, person.y, person_2.y) < self.safe_distance:
-                                too_close.add([person, person_2])
+                                too_close.add((person, person_2))
         return too_close
 
 
-
-
-
-
-
-
 class Population:
-    def __init__(self, n, area):
+    def __init__(self, n, area, standard_distance, standard_velocity, infected_ratio):
         self.size = 0
         self.distribution = {}
         self.distribution["Susceptible"] = 0
@@ -160,15 +158,22 @@ class Population:
         self.distribution["Vaccinated"] = 0
         self.population_list = []
         self.area = area
-        self.vel = 2
-        self.rot_vel = self.vel*np.pi/15
-        for i in range(int(9*n/10)):
+        self.distance = standard_distance/n**(1/2)
+        print(self.distance)
+        self.population_matrix = PopulationMatrix(self.area, self.distance)
+        self.vel = standard_velocity/n**(1/2)
+        self.rot_vel = np.pi/15
+        inf = math.ceil(max(1, n*infected_ratio))
+        for i in range(n-inf):
             self.add_person(rnd.randint(0, self.area.width), rnd.randint(0, self.area.height), 0)
-        for i in range(n-int(9*n/10)):
+        for i in range(inf):
             self.add_person(rnd.randint(0, self.area.width), rnd.randint(0, self.area.height), 1)
 
+
     def add_person(self, x, y, sick):
-        self.population_list.append(Person(x, y, self.vel, 2 * np.pi * rnd.random(), self.rot_vel, sick, self.area.tp_spot, self.area.tp_radius))
+        p = Person(x, y, self.vel, 2 * np.pi * rnd.random(), self.rot_vel, sick, self.area.tp_spot, self.area.tp_radius)
+        self.population_list.append(p)
+        self.population_matrix.add_person(p)
         self.size += 1
         if sick:
             self.distribution["Infected"] += 1
@@ -181,13 +186,13 @@ class Population:
 
 
 class Area:
-    def __init__(self, x, y, w, h, tp_spot, tp_radius):
+    def __init__(self, x, y, w, h, tp_spot, radius, n):
         self.x = x
         self.y = y
         self.width = w
         self.height = h
         self.tp_spot = tp_spot
-        self.tp_radius = tp_radius
+        self.tp_radius = radius
         self.market_text = font2.render("SUPERMARKET", True, (255,0,0))
 
     def draw(self):
@@ -195,16 +200,14 @@ class Area:
         pygame.draw.rect(screen, (0, 0, 0), (self.x, self.y, self.width, self.height), 2)
         pygame.draw.circle(screen, (255,200,200), (self.x+self.tp_spot[0], self.y+self.tp_spot[1]), self.tp_radius)
         pygame.draw.circle(screen, (255,0,0), (self.x+self.tp_spot[0], self.y+self.tp_spot[1]), self.tp_radius, 1)
-
         screen.blit(self.market_text, (self.x+self.tp_spot[0]-self.market_text.get_width()/2, self.y+self.tp_spot[1]-self.market_text.get_height()/2))
-
 
 
 class Manager:
     def __init__(self, population):
         self.population = population
         self.inf_prob = 0.01
-        self.distance = 50
+        self.distance = self.population.distance
         self.colors = [(0,0,0),(255,0,0),(0,0,255),(100,100,100)]
 
     def update(self, population, graphics):
@@ -218,14 +221,20 @@ class Manager:
                 screen.blit(text, (0, 25*(i+1)))
 
         #Undersöker om smittspridning kan ske
-        for person in population.population_list:
-            for person2 in population.population_list:
-                if distance(person.x, person2.x, person.y, person2.y) <= self.distance and person.state == 1 and person2.state == 0:
-                    if rnd.random()<self.inf_prob:
-                        self.infect(person2)
+        close_persons = population.population_matrix.check_distance(population.population_list)
+        for pair in close_persons:
+            if (pair[0].state == 1 and pair[1].state == 0):
+                if rnd.random()<self.inf_prob:
+                    self.infect(pair[1])
 
+            elif (pair[0].state == 0 and pair[1].state == 1):
+                if rnd.random()<self.inf_prob:
+                    self.infect(pair[0])
+
+        for person in population.population_list:
             #Kör update för varje person
             state = person.update(population.area)
+            self.population.population_matrix.update_person(person)
             if state == 1:
                 population.distribution["Infected"] -= 1
                 population.distribution["Recovered"] += 1
@@ -233,7 +242,7 @@ class Manager:
                 population.distribution["Infected"] -= 1
                 population.distribution["Dead"] += 1
             if graphics:
-                person.draw(population.area, self.colors)
+                person.draw(population.area, self.colors, population.population_matrix.safe_distance)
 
     def infect(self, person):
         self.population.distribution["Susceptible"] -= 1
@@ -257,39 +266,51 @@ class Stats:
                 plt.plot(self.data[key])
             plt.show()
 
-    def plot(self):
-        plt.plot(self.I)
+    def current_stats(self):
+        s = ""
+        for key in self.data.keys():
+            s += key + ": " + str(self.data[key][-1]) + "\n"
+        return s
 
 
 def main():
+    global graphics
     graphics = True
-    area = Area(100, 150, 600, 400, [200,200], 50)
-    population = Population(50, area)
+    n = 100
+    std_distance = 350
+    std_velocity = 14
+    area = Area(100, 150, 600, 400, [200,200], 50, n)
+    population = Population(n, area, std_distance, std_velocity, 0.001)
     manager = Manager(population)
     stats = Stats(population)
-
     #Huvudloop där allt uppdateras
-    while True:
-        if graphics:
+    if graphics:
+        while True:
             screen.fill((255, 255, 255))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-                if area.width > x-area.x > 0 and area.height > y-area.y > 0:
-                    if event.button == 1:
-                        population.add_person(x-area.x, y-area.y, False)
-                    if event.button == 3:
-                        population.add_person(x-area.x, y-area.y, True)
-        if graphics:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    if area.width > x-area.x > 0 and area.height > y-area.y > 0:
+                        if event.button == 1:
+                            population.add_person(x-area.x, y-area.y, False)
+                        if event.button == 3:
+                            population.add_person(x-area.x, y-area.y, True)
             area.draw()
-        manager.update(population, graphics)
-        stats.update()
-        if graphics:
+            manager.update(population, graphics)
+            stats.update()
             pygame.display.update()
             clock.tick(fps)
-
+    else:
+        frames = 0
+        while True:
+            manager.update(population, graphics)
+            stats.update()
+            frames += 1
+            if frames % 15 == 0:
+                print(frames)
+                print(stats.current_stats())
 
 main()
